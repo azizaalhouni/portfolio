a96b092d97d667cc16406a2b010d14a8d2f06c01
aziza alhouni <azizaalhouni@yahoo.com>
2016-07-28 09:06:08 -0700

    heroku2

--- js/models/repo.js
+++ js/models/repo.js
@@ -3,20 +3,25 @@
   var reposObj = {};
   reposObj.allRepos = [];
   reposObj.requestRepos = function(next) {
-    //fetch the repo
-    $.ajax({
-      url: 'https://api.github.com/users/azizaalhouni/repos',
-      type: 'GET',
-      headers: {
-        'Authorization':'token '+ token,
-      },
-      success: function(data, message, xhr) {
-        data.forEach(function(current) {
-          reposObj.allRepos.push(current);
-        });
-        next();
-      }
-    });
+    // //fetch the repo
+    // $.ajax({
+    //   url: 'https://api.github.com/users/azizaalhouni/repos',
+    //   type: 'GET',
+    //   headers: {
+    //     'Authorization':'token '+ token,
+    //   },
+    //   success: function(data, message, xhr) {
+    //     data.forEach(function(current) {
+    //       reposObj.allRepos.push(current);
+    //     });
+    //     next();
+    //   }
+    // });
+    $.get('/github/users/aizaalhouni/repos' + '?per_page=10&sort=updated')
+    .done(function(data) {
+      reposObj.allRepos = data;
+    }).done(next);
+
   };
   reposObj.withTheAttribute = function(myAttr) {
     return reposObj.allRepos.filter(function(aRepo) {
